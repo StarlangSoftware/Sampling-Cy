9722858470a8f684de818fe0477597bf7ef0bea7
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-20 11:18:19 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["Sampling/*.pyx", "Sampling/*.pxd"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-Sampling-Cy',
-    version='1.0.5',
+    version='1.0.6',
     packages=['Sampling'],
     package_data={'Sampling': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/StarlangSoftware/Sampling-Cy',
